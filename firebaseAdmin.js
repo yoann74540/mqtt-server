1592fa2fa5f095e9e173275f92ad6b8c72acc3f7
yoann74540 <dezeuze@hotmail.fr>
2026-01-04 09:51:05 +0100

    clean code with firebaseAdmin file

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -6,4 +6,20 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
-export const auth = admin.auth();
+const auth = admin.auth();
+
+export async function verifyFirebaseToken(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader?.startsWith("Bearer ")) return res.status(401).json({ error: "No token provided" });
+
+    const idToken = authHeader.split("Bearer ")[1];
+
+    try{
+        const decodedToken = await auth.verifyIdToken(idToken);
+        console.log("UID de l'utilisateur :", decodedToken.uid);
+        req.user = decodedToken;
+        next();
+    }catch (err){
+        return res.status(403).json({ error: "Token invalide"})
+    }
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mqtt from "mqtt";
 import cors from "cors";
-import {auth} from "./firebaseAdmin.js"
+import {verifyFirebaseToken} from "./firebaseAdmin.js"
 
 const app = express();
 app.use(cors());
@@ -12,48 +12,41 @@ const{
     AIO_KEY
 } = process.env;
 
-app.post("/heater", async (req, res) => {
+app.post("/heater", verifyFirebaseToken, async (req, res) => {
+
     console.log("Headers", req.headers);
     console.log("Body reçu", req.body);
+
     const {state} = req.body;
 
-    const idToken = req.headers.authorization?.split("Bearer ")[1];
-    if(!idToken) return res.status(401).json({ error: "No token provided" });
+    console.log("command de", req.user.email, "->", state);
 
-    try{
-        const decodedToken = await auth.verifyIdToken(idToken);
-        console.log("UID de l'utilisateur :", decodedToken.uid);
-
-        if(typeof state !== "boolean"){
-            return res.status(400).json({
-                error: "Invalid state",
-                received: req.body
-            });
-        }
-
-        const topic = `${AIO_USERNAME}/feeds/chauffage`;
-
-        const client = mqtt.connect("mqtts://io.adafruit.com", {
-            username: AIO_USERNAME,
-            password: AIO_KEY
+    if(typeof state !== "boolean"){
+        return res.status(400).json({
+            error: "Invalid state",
+            received: req.body
         });
-
-        client.on("connect", () => {
-            client.publish(topic, state ? "ON" : "OFF", {}, () => {
-                setTimeout(() => client.end(), 300);
-                res.json({ success: true });
-            });
-        });
-
-        client.on("error", err => {
-            client.end();
-            res.status(500).json({ error: err.message });
-        });
-
-    }catch(err){
-        console.error(err);
-        res.status(403).json({ error : "Token invalide"});
     }
+
+    const topic = `${AIO_USERNAME}/feeds/chauffage`;
+
+    const client = mqtt.connect("mqtts://io.adafruit.com", {
+        username: AIO_USERNAME,
+        password: AIO_KEY
+    });
+
+    client.on("connect", () => {
+        client.publish(topic, state ? "ON" : "OFF", {}, () => {
+            setTimeout(() => client.end(), 300);
+            res.json({ success: true });
+        });
+    });
+
+    client.on("error", err => {
+        client.end();
+        res.status(500).json({ error: err.message });
+    });
+
 });
 
 const PORT = process.env.PORT || 3000;
